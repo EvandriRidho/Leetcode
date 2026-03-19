b0b9e547502f03a6571a44603b59ecdfbb6d8edd
EvandriRidhoHasmono <evandriridho555@gmail.com>
2026-03-19 17:23:40 +0700

    twosum easy solved

---
+++ Easy/two-sum/main.js
@@ -0,0 +1,17 @@
+const nums = [2, 7, 11, 15]
+const target = 9;
+
+// Brute Force
+const twoSum = (arr, target) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+}
+
+
+
+console.log(twoSum(nums, target)) // Compleks O(n^2)
